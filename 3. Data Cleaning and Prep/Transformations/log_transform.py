f686e868c82d4e5375c35e8adba63ce19497f6be
Liana Principe <69564111+lap309@users.noreply.github.com>
2024-03-02 19:51:42 -0500

    Create log_transform.py

---
+++ 3. Data Cleaning and Prep/Transformations/log_transform.py
@@ -0,0 +1,14 @@
+"""Good for linear regression if the raw data is not actually linear or if the data has diminishing returns"""
+
+from numpy import log, log1p
+from scipy.stats.import boxcox
+import seaborn as sns
+
+#use log1p if you have 0s in your dataset: log1p is the log+1 because you can't take the log of 0
+#boxcox will find the ideal way to transform from a skewed dataset (left/right skewed)
+
+#Apply log transformation
+log_data = [math.log(x) for x in data['col_name']]
+
+# plot transformed plots
+sns.displot(log_data, bins=20)
