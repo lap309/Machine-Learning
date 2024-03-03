f7da75012703fb4ae186e90e6b80fa88ca52a80d
Liana Principe <69564111+lap309@users.noreply.github.com>
2024-03-02 23:06:52 -0500

    Update Finding NANs.py

--- 3. Data Cleaning and Prep/Null Values/Finding NANs.py
+++ 3. Data Cleaning and Prep/Null Values/Finding NANs.py
@@ -10,3 +10,28 @@ def find_nulls(df):
     plt.ylabel('Percent of missing values', fontsize=15)
     plt.title('Percent missing data by feature', fontsize=15)
     return nulls_df
+
+######## IMPUTING WITH LINEAR REGRESSION
+### Takes the data frame, x, and y, and will calculate the mean 
+nulls = df['MINIMUM_PAYMENTS'].isnull()
+not_nulls = ~nulls
+
+target = df['MINIMUM_PAYMENTS'][not_nulls]
+others = list(col for col in df.columns if col not in ['MINIMUM_PAYMENTS','CUST_ID'])
+features = df[others][not_nulls]
+
+
+from sklearn.linear_model import LinearRegression
+
+def impute_lin_reg(df, x, y):
+  '''need find_na function (Machine-Learning/Null Values/find_na)
+  Input:
+  df = dataframe
+  x = independent variables
+  y = dependent variable
+
+  '''
+  lin_reg = LinearRegression()
+  lin_reg.fit(x, y)
+  lin_reg.predict(nulls)
+  lin_reg.score(features[not_nulls], target)
