ec6f837653acee158a91b2cedeb8187f50f94c3d
Liana Principe <69564111+lap309@users.noreply.github.com>
2024-03-16 14:36:45 -0400

    Update encoding.py

--- 3. Data Modeling Prep/encoding.py
+++ 3. Data Modeling Prep/encoding.py
@@ -7,3 +7,9 @@ from pandas import get_dummies
 # encoding ordinal/ordered variables
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import OrdinalEncoder
+
+### Label Encoding with .replace()
+df['col'].replace({'old_val1': 'new_val1', 
+           'old_val2' : 'new_val2',
+           'old_val3': 'new_val3'}, 
+           inplace= True)
