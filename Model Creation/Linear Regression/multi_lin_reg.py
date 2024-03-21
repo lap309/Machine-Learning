f9230b11e712c1ec2f728895fcd867b56ce9edb7
Liana Principe <69564111+lap309@users.noreply.github.com>
2024-03-20 21:41:46 -0400

    Update and rename Multi-Linear Regression Statsmodel to multi_lin_reg.py

--- Model Creation/Linear Regression/multi_lin_reg.py
+++ Model Creation/Linear Regression/multi_lin_reg.py
@@ -1,4 +1,20 @@
+import pandas as pd
 import statsmodel.formula.api as smf
+
+###### Assess CORRELATIONS
+def correlations(data, target_var, print_results=True):
+  sig_column = []
+  nonsig_column = []
+  for col in data.select_dtypes('number').columns:
+      p_value = stats.pearsonr(data[col],data[target_var])
+      if p_value[1]< 0.05:
+         sig_column.append(col)print(col, p_value[1])
+      else:
+        nonsig_column.append(col)
+      if print_results==True:
+        print(col, p_value[1])
+  print('Columns of Significance: ', sig_column)
+  print('Non-significant: ',nonsig_column)
 
 # define a string formula using column names (column names can't have whitespace)
 formula = 'y_col ~ x_col1 + x_col2 + C(x_col3)'   # C(x_col3) indicates that x_col3 is a a categorical variable
