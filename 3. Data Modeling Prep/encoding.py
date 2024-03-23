ecbf0d9c1f92916fabfebe3dcae4ab293e28615a
Liana Principe <69564111+lap309@users.noreply.github.com>
2024-03-23 19:18:21 -0400

    Update encoding.py

--- 3. Data Modeling Prep/encoding.py
+++ 3. Data Modeling Prep/encoding.py
@@ -1,8 +1,16 @@
 #encoding nominal data
-from sklearn.preprocessing import LabelEncoder, LabelBinarizer, OneHotEncoder
-from pandas import get_dummies
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer
+
 
 #one hot encoder will remove the base column while get_dummies will not
+
+from sklearn.preprocessing import OneHotEncoder
+
+
+from pandas import get_dummies
+dummies = pd.get_dummies(data['col'], drop_first = True)           #create the dummies
+data = pd.concat([data, dummies], axis = 1)                      # add the dummies to the original df
+data.drop(['col'], axis = 1, inplace = True)                         # drop the original column
 
 # encoding ordinal/ordered variables
 from sklearn.feature_extraction import DictVectorizer
