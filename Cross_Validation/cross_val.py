1b9485e3aa6407ab372e79387465bcd0261e9b62
Liana Principe <69564111+lap309@users.noreply.github.com>
2024-03-24 16:49:30 -0400

    Update and rename K-fold to cross_val.py

--- Cross_Validation/cross_val.py
+++ Cross_Validation/cross_val.py
@@ -4,8 +4,21 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.pipeline import Pipeline
 
 # define dataset
-X = x_train
-y = y_train
+X_data = data.drop('target_var', axis=1)
+y_data = data['target_var']
+
+estimator = Pipeline([("scaler", StandardScaler()),
+                      ("regression", LinearRegression())])
+
+kf = KFold(shuffle=True, random_state=72018, n_splits=3)  # shuffling the indexis of the rows rather than going in order or in a pattern
+
+#cross_val_predict is a function that does K-fold cross validation for us, appropriately fitting and transforming at every step of the way
+predictions = cross_val_predict(estimator, X_data, y_data, cv=kf)  # could also put cv = # of splits. ex: cv = 3 (the data just might not be shuffled)
+
+
+
+
+
 
 # define the pipeline
 steps = list()
