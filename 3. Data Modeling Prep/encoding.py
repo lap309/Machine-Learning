b295dba9c8570c7db3972c642a775a3b30aeaaa9
Liana Principe <69564111+lap309@users.noreply.github.com>
2024-03-23 23:39:22 -0400

    Update encoding.py

--- 3. Data Modeling Prep/encoding.py
+++ 3. Data Modeling Prep/encoding.py
@@ -2,12 +2,25 @@
 from sklearn.preprocessing import LabelEncoder, LabelBinarizer
 
 
+# Determine how many extra columns would be created
+category_columns = data.columns[data.dtypes == object]
+num_ohc_cols = (data[categorical_columns]
+                .apply(lambda x: x.nunique())
+                .sort_values(ascending=False))
+
+small_num_ohc_cols = num_ohc_cols.loc[num_ohc_cols>1]     # No need to encode if there is only one value
+small_num_ohc_cols -= 1                # Number of one-hot columns is one less than the number of categories
+small_num_ohc_cols.sum()           #prints the number of columns that would be created if one hot encoded
+
+
 #one hot encoder will remove the base column while get_dummies will not
 
 from sklearn.preprocessing import OneHotEncoder
 
 
 from pandas import get_dummies
+
+
 dummies = pd.get_dummies(data['col'], drop_first = True)           #create the dummies
 data = pd.concat([data, dummies], axis = 1)                      # add the dummies to the original df
 data.drop(['col'], axis = 1, inplace = True)                         # drop the original column
