bcf37b76374a753900c3caae78e572bdcf1b7684
Liana Principe <69564111+lap309@users.noreply.github.com>
2024-03-26 00:25:21 -0400

    Update feature_selection.py

--- 3. Data Modeling Prep/feature_selection.py
+++ 3. Data Modeling Prep/feature_selection.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-##### Correlation Coefficient and Heatmap
+################## Correlation Coefficient and Heatmap
 plt.figure(figsize = (18,18))
 sns.heatmap(data.corr(),    # calculates correlation coefficient
             annot=True,    # gives the value of the correlation coefficient as well as the color
@@ -15,3 +15,12 @@ plt.show()
 ### Correlation Coefficients and bar plot
 df_corr = data.corr()['target_var'].sort_values()
 df_corr.plot(kind='bar', figsize = (10,8))
+
+################### Recursive Feature Elimination (REF)
+from sklearn.feature_Selection import RFE
+
+rfe_mod = RFE( est = ml_model, n_features_to_select = 15 )          #instantiate
+
+#the model can be a linear regression, a lasso object, or a more complex object. It just needs to have feature importances or coefficients
+rfe_mod=rfe_mod.fit(x_train, y_train)                           # fit
+y_pred = rfe_mod.predict(x_test)                                   # predict
