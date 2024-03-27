f923c608736495b4ab8c35f68fc5f8c8316389d3
Liana Principe <69564111+lap309@users.noreply.github.com>
2024-03-26 21:47:06 -0400

    Update correlations.py

--- 2. Data Cleaning and EDA/Correlation/correlations.py
+++ 2. Data Cleaning and EDA/Correlation/correlations.py
@@ -1,4 +1,4 @@
-### Create a correlation coefficient
+#Identify correlations
 df_num= df.select_dtypes(include = ['float64', 'int64'])
 df_corr = df_num.corr()['target_var'][:-1] # -1 means that the latest row is SalePrice
 top_features = df_corr[abs(df_corr) > 0.5].sort_values(ascending=False) #displays pearsons correlation coefficient greater than 0.5
@@ -9,3 +9,6 @@ for i in range(0, len(df_num.columns), 5):
     sns.pairplot(data=df_num,
                 x_vars=df_num.columns[i:i+5],
                 y_vars=['target_var'])
+
+#Create the correlation matrix
+corr_values = df_num.corr()
