ea7d510dce4a5fb096c71e73aa5d56bc61cbc2a1
Liana Principe <69564111+lap309@users.noreply.github.com>
2025-03-16 17:03:45 -0400

    Update matplotlib_graphs.py

--- 2. Data Cleaning and EDA/matplotlib/matplotlib_graphs.py
+++ 2. Data Cleaning and EDA/matplotlib/matplotlib_graphs.py
@@ -1,12 +1,20 @@
 import numpy as np
-
-
-#Note**: A good rule of thumb when deciding the number of bins is to take the square root of the number of observations
-
-
-
-########### MATPLOTLIB ##################
 import matplotlib.pyplot as plt
+
+########### Histogram ##################
+#Note**: A good rule of thumb when deciding the number of bins is to take the square root of the number of observations
 n_bins = int(np.sqrt(len(df)))      #takes the square root of the number of rows and converts to an integer
 plt.figure(figsize = (8,4))
 plt.hist(df, bins = n_bins, color = 'green')
+
+########### Scatterplot ##################
+# helps see direction and relationship of data
+# outliers will be the highest or lowest points in the plot
+#Univariate data
+integers = range(len(df))    #creates a list of consecutive integers with the same length as our distribution
+plt.figure(figsize=(16,18))
+plot.scatter(integers, df, c = 'blue', alpha = 0.5)
+
+#Bivariate data
+plt.figure(figsize=(16,18))
+plot.scatter(df['x_col'], df['y_col'], c = 'blue', alpha = 0.5)
