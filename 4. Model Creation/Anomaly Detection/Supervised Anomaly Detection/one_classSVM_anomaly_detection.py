044bc2503dc2de4a3fc602bb8452ec328a76221a
Liana Principe <69564111+lap309@users.noreply.github.com>
2025-03-19 01:40:29 -0400

    Update one_classSVM_anomaly_detection.py

--- 4. Model Creation/Anomaly Detection/Supervised Anomaly Detection/one_classSVM_anomaly_detection.py
+++ 4. Model Creation/Anomaly Detection/Supervised Anomaly Detection/one_classSVM_anomaly_detection.py
@@ -3,17 +3,22 @@ import matplotlib.pyplot as plt
 from sklearn.svm import OneClassSVM
 
 # Initialize and fit the One-Class SVM model
-clf = OneClassSVM(nu=0.05, kernel="rbf")  # Adjust the nu parameter as needed
-clf.fit(data)
+svm = OneClassSVM(nu=0.05, kernel="rbf")  # Adjust the nu parameter as needed
+svm.fit(data)
 
 # Predict whether each data point is an anomaly (-1 for anomalies, 1 for normal data)
-predictions = clf.predict(data)
+predictions = svm.predict(data)
 
 # Visualize the results
-plt.scatter(data[:, 0], data[:, 1], c=predictions, cmap='viridis')
-plt.colorbar(label="Anomaly Score")
-plt.title("Anomaly Detection using One-Class SVM")
-plt.show()
+def plot_anomaly_detection_results(predictions, title):
+    plt.scatter(data[:, 0], data[:, 1], c=predictions, cmap='viridis')
+    plt.colorbar(label="Anomaly Score")
+    plt.title(title)
+    plt.show()
+
+plot_anomaly_detection_results(predictions, "Anomaly Detection using One Class SVM")
+
+
 
 # Identify anomalies (outliers)
 anomalies_indices = np.where(predictions == -1)[0]
