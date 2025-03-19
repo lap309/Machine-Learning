0d75db6c796b3a7f2c37ac7ba4e86f5e3b45d787
Liana Principe <69564111+lap309@users.noreply.github.com>
2025-03-19 02:02:40 -0400

    Update isolation_forest_anomaly_detection.py

--- 4. Model Creation/Anomaly Detection/Supervised Anomaly Detection/isolation_forest_anomaly_detection.py
+++ 4. Model Creation/Anomaly Detection/Supervised Anomaly Detection/isolation_forest_anomaly_detection.py
@@ -1,11 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
 
-# Initialize and fit Isolation Forest for anomaly detection
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Initialize and fit Isolation Forest for training anomaly detection
 iso_forest = IsolationForest(contamination=0.05, random_state=42)
-iso_forest.fit(data)
-iso_forest_predictions = iso_forest.predict(data)
+iso_forest.fit(X_train)
+train_pred = iso_forest.predict(X_train)
+
+print("Confusion Matrix: \n", confusion_matrix(y_train, train_pred))
+print("Classification Report:\n", classification_report(y_train, train_pred))
+
+
+#Apply to test set
+test_pred = iso_forest.predict(X_test)
+
+# Evaluate the test set
+print("Confusion Matrix: \n", confusion_matrix(y_test, test_pred))
+print("Classification Report:\n", classification_report(y_test, test_pred))
 
 # Visualize the results
 def plot_anomaly_detection_results(predictions, title):
@@ -14,5 +30,8 @@ def plot_anomaly_detection_results(predictions, title):
     plt.title(title)
     plt.show()
 
-plot_anomaly_detection_results(dt_predictions, "Anomaly Detection using Decision Trees")
+plot_anomaly_detection_results(test_predictions, "Anomaly Detection using Isolation Forest")
+
+
+
 
